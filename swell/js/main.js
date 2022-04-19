3b0eb304eb2983fd75d5ace83576656eb045a37d
KirillShpakov <99267268+KirillShpakov@users.noreply.github.com>
2022-04-19 09:51:18 +0300

    init

---
+++ swell/js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+    $('.favorite').click(function () {
+        $(this).toggleClass("active");
+    })
+});
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+                  
